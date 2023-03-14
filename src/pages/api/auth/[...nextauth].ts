b782cd084958509f7243a51a0b8d2573d58574c0
piercenovo <piercenovoah@gmail.com>
2023-03-14 13:56:44 -0500

    feat: add database creation with Prisma and PostgreSQL

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,16 @@
+import NextAuth from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import prismadb from '@/lib/prismadb'
+
+export const authOptions = {
+  adapter: PrismaAdapter(prismadb),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || ''
+    })
+  ]
+}
+
+export default NextAuth(authOptions)
