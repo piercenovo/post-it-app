5950c7ccc34106f71227cbe757c9ce65a47210a3
piercenovo <piercenovoah@gmail.com>
2023-03-15 02:32:46 -0500

    refactor: add custom hook to createPost

--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -1,47 +1,9 @@
 'use client'
 
-import React, { useState } from 'react'
-import { useMutation, useQueryClient } from '@tanstack/react-query'
-import axios, { AxiosError } from 'axios'
-import toast from 'react-hot-toast'
+import { useCreatePost } from '@/hooks/useCreatePost'
 
 export default function AddPost () {
-  const [title, setTitle] = useState('')
-  const [isDisabled, setIsDisabled] = useState(false)
-  const queryClient = useQueryClient()
-  let toastPostID: string
-
-  const handleChange = (event: any) => {
-    setTitle(event.target.value)
-  }
-
-  // Create a post
-  const { mutate } = useMutation(
-    async (title: string) =>
-      await axios.post('/api/posts/addPost', {
-        title
-      }),
-    {
-      onError: (error) => {
-        if (error instanceof AxiosError) {
-          toast.error(error?.response?.data.message, { id: toastPostID })
-        }
-        setIsDisabled(false)
-      },
-      onSuccess: (data) => {
-        queryClient.invalidateQueries(['posts'])
-        toast.success('Post has been made 🔥', { id: toastPostID })
-        setTitle('')
-        setIsDisabled(false)
-      }
-    }
-  )
-  const submitPost = async (e: React.FormEvent) => {
-    e.preventDefault()
-    setIsDisabled(true)
-    toastPostID = toast.loading('Creating your post', { id: toastPostID })
-    mutate(title)
-  }
+  const { isDisabled, title, handleChange, submitPost } = useCreatePost()
 
   return (
     <form onSubmit={submitPost} className='bg-white my-8 p-8 rounded-md'>
